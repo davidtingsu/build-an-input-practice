eaa6f25cf97f493872a9f21e3e65f5a59de8a092
David Su <davidtingsu@gmail.com>
2021-12-19 13:26:22 +0800

    add disabled styling

--- src/app.js
+++ src/app.js
@@ -4,7 +4,10 @@ import { CustomInputNumber } from "./components/custom-input-number";
 export const App = () => {
   return (
     <>
+      <h1>Normal Example</h1>
       <CustomInputNumber value={2} />
+      <h1>Disabled Example</h1>
+      <CustomInputNumber disabled={true} value={2} />
     </>
   );
 };
--- src/components/custom-input-number.js
+++ src/components/custom-input-number.js
@@ -11,9 +11,9 @@ export const CustomInputNumber = ({
 }) => {
   return (
     <div data-component="number-input">
-      <button data-component="element" className={'minus symbol'}/>
-      <input data-component="element" className={'value'} value={value}/>
-      <button data-component="element" className={'plus symbol'}><div/></button>
+      <button data-component="element" className={`minus symbol ${disabled ? 'disabled': ''}`}/>
+      <input disabled={disabled} data-component="element" className={'value'} value={value}/>
+      <button data-component="element" className={`plus symbol ${disabled ? 'disabled': ''}`}><div/></button>
     </div>
   );
 };
